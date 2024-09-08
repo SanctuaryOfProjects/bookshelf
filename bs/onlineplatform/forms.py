ff8c9ecdcbd10807e083c2416aa9bef926f77a9c
patau23 <akejan_2302@mail.ru>
2024-09-08 11:07:07 +0500

    ake

--- bs/onlineplatform/forms.py
+++ bs/onlineplatform/forms.py
@@ -24,30 +24,109 @@ class UserProfileForm(forms.ModelForm):
         model = UserProfile
         fields = ['full_name', 'photo', 'birth_date', 'phone_number', 'reader_ticket', 'favorite_categories']
         widgets = {
-            'birth_date': forms.DateInput(attrs={'type': 'date'}),
+            'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Полное имя'}),
+            'photo': forms.FileInput(attrs={'class': 'form-control'}),
+            'birth_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Номер телефона'}),
+            'reader_ticket': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Читательский билет'}),
+            'favorite_categories': forms.SelectMultiple(attrs={'class': 'form-control'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Применение классов Bootstrap ко всем полям
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control mb-3'})
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['full_name', 'subject', 'club', 'contact_method', 'contact_info', 'message']
-    
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваше полное имя'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Тема'}),
+            'club': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название клуба'}),
+            'contact_method': forms.Select(attrs={'class': 'form-control'}),
+            'contact_info': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Контактная информация'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Ваше сообщение'}),
+        }
+
     def __init__(self, *args, **kwargs):
         show_club = kwargs.pop('show_club', False)
         super().__init__(*args, **kwargs)
         if not show_club:
             self.fields.pop('club')
 
+        # Применение классов Bootstrap ко всем полям
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control mb-3'})
+
 
 class BookSearchForm(forms.Form):
-    title = forms.CharField(required=False, label='Название книги')
-    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False, label='Категория')
-    author = forms.CharField(required=False, label='Авторы')
-    publisher = forms.CharField(required=False, label='Издатель')
-    language = forms.CharField(required=False, label='Язык')
-    format = forms.ChoiceField(choices=Book.FORMAT_CHOICES, required=False, label='Формат')
-    publication_date_from = forms.DateField(required=False, label='Дата публикации от', widget=forms.TextInput(attrs={'type': 'date'}))
-    publication_date_to = forms.DateField(required=False, label='Дата публикации до', widget=forms.TextInput(attrs={'type': 'date'}))
+    title = forms.CharField(
+        required=False,
+        label='Название книги',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Bootstrap класс для input
+            'placeholder': 'Название книги'  # Добавить плейсхолдер
+        })
+    )
+    category = forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        required=False,
+        label='Категория',
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        })
+    )
+    author = forms.CharField(
+        required=False,
+        label='Авторы',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Авторы'
+        })
+    )
+    publisher = forms.CharField(
+        required=False,
+        label='Издатель',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Издатель'
+        })
+    )
+    language = forms.CharField(
+        required=False,
+        label='Язык',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Язык'
+        })
+    )
+    format = forms.ChoiceField(
+        choices=Book.FORMAT_CHOICES,
+        required=False,
+        label='Формат',
+        widget=forms.Select(attrs={
+            'class': 'form-control'
+        })
+    )
+    publication_date_from = forms.DateField(
+        required=False,
+        label='Дата публикации от',
+        widget=forms.TextInput(attrs={
+            'type': 'date',
+            'class': 'form-control'
+        })
+    )
+    publication_date_to = forms.DateField(
+        required=False,
+        label='Дата публикации до',
+        widget=forms.TextInput(attrs={
+            'type': 'date',
+            'class': 'form-control'
+        })
+    )
 
 class ReviewForm(forms.ModelForm):
     class Meta:
